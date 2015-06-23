6b4b03162bec1f5a2f456a24777984994ce57093
NadiaUQAM <nadia_tahiri@yahoo.fr>
2015-06-23 10:22:09 -0400

    Lab7

---
+++ Lab/Lab7_sol/creParc.sql
@@ -0,0 +1,51 @@
+-- Creation des tables et PK
+
+CREATE TABLE Segment
+	( seqSeg    NUMBER(5),
+	 indIP      VARCHAR2(11),
+	 nomSegment VARCHAR2(20) CONSTRAINT nn_nomSegment NOT NULL,
+	 etage NUMBER(2),
+	 CONSTRAINT pk_Segment PRIMARY KEY (seqSeg));
+
+CREATE TABLE Salle
+	(seqSalle   NUMBER(5),
+	 nSalle     VARCHAR2(7),
+	 nomSalle   VARCHAR2(20) CONSTRAINT nn_nomSalle NOT NULL,
+	 nbPoste    NUMBER(2),
+	 indIP      VARCHAR2(11),
+	 CONSTRAINT pk_salle PRIMARY KEY (seqSalle));
+
+CREATE TABLE Poste
+	(sqqPoste   NUMBER(5),
+	 nPoste     VARCHAR2(7),
+	 nomPoste   VARCHAR2(20) CONSTRAINT nn_nomPoste NOT NULL,
+	 indIP      VARCHAR2(11),
+	 ad         VARCHAR2(3),
+	 typePoste  VARCHAR2(9),
+	 nSalle     VARCHAR2(7),
+	 CONSTRAINT pk_Poste PRIMARY KEY (sqqPoste),
+	 CONSTRAINT ck_ad    CHECK (ad BETWEEN '000' AND '255'));
+
+CREATE TABLE Logiciel
+	(seqLog     NUMBER(5),
+	 nLog       VARCHAR2(5),
+	 nomLog     VARCHAR2(20) CONSTRAINT nn_nomLog NOT NULL,
+	 dateAch    DATE,
+	 version    VARCHAR2(7),
+	 typeLog    VARCHAR2(9),
+	 prix       NUMBER(6,2),
+	 CONSTRAINT pk_Logiciel PRIMARY KEY (seqLog),
+	 CONSTRAINT ck_prix     CHECK (prix >= 0));
+
+CREATE TABLE Installer
+	(nPoste     VARCHAR2(7),
+	 nLog       VARCHAR2(5),
+	 numIns     NUMBER(5),
+	 dateIns    DATE DEFAULT SYSDATE,
+	 delai      INTERVAL DAY(5) TO SECOND(2),
+	 CONSTRAINT pk_Installer PRIMARY KEY(nPoste,nLog));
+
+CREATE TABLE Types
+	(seqTyp   NUMBER(5),
+	 typeLP   VARCHAR2(9), nomType VARCHAR2(20),
+	 CONSTRAINT pk_types PRIMARY KEY(seqTyp));
---
+++ Lab/Lab7_sol/insParc.sql
@@ -0,0 +1,71 @@
+--
+
+INSERT INTO Segment VALUES (sequenceSeg.NEXTVAL,'130.120.80','Brin RDC',NULL);
+INSERT INTO Segment VALUES (sequenceSeg.NEXTVAL,'130.120.81','Brin 1er  étage',NULL);
+INSERT INTO Segment VALUES (sequenceSeg.NEXTVAL,'130.120.82','Brin 2ème étage',NULL);
+
+INSERT INTO Salle VALUES (sequenceSalle.NEXTVAL,'s01','Salle 1',3,'130.120.80');
+INSERT INTO Salle VALUES (sequenceSalle.NEXTVAL,'s02','Salle 2',2,'130.120.80');
+INSERT INTO Salle VALUES (sequenceSalle.NEXTVAL,'s03','Salle 3',2,'130.120.80');
+INSERT INTO Salle VALUES (sequenceSalle.NEXTVAL,'s11','Salle 11',2,'130.120.81');
+INSERT INTO Salle VALUES (sequenceSalle.NEXTVAL,'s12','Salle 12',1,'130.120.81');
+INSERT INTO Salle VALUES (sequenceSalle.NEXTVAL,'s21','Salle 21',2,'130.120.82');
+INSERT INTO Salle VALUES (sequenceSalle.NEXTVAL,'s22','Salle 22',0,'130.120.83');
+INSERT INTO Salle VALUES (sequenceSalle.NEXTVAL,'s23','Salle 23',0,'130.120.83');
+
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p1','Poste 1','130.120.80','01','TX','s01');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p2','Poste 2','130.120.80','02','UNIX','s01');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p3','Poste 3','130.120.80','03','TX','s01');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p4','Poste 4','130.120.80','04','PCWS','s02');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p5','Poste 5','130.120.80','05','PCWS','s02');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p6','Poste 6','130.120.80','06','UNIX','s03');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p7','Poste 7','130.120.80','07','TX','s03');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p8','Poste 8','130.120.81','01','UNIX','s11');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p9','Poste 9','130.120.81','02','TX','s11');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p10','Poste 10','130.120.81','03','UNIX','s12');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p11','Poste 11','130.120.82','01','PCNT','s21');
+INSERT INTO poste VALUES (sequencePoste.NEXTVAL,'p12','Poste 12','130.120.82','02','PCWS','s21');
+
+INSERT INTO logiciel VALUES (sequenceLog.NEXTVAL,'log1','Oracle 6',   '13-05-1995','6.2','UNIX',3000);
+INSERT INTO logiciel VALUES (sequenceLog.NEXTVAL,'log2','Oracle 8',   '15-09-1999','8i','UNIX',5600);
+INSERT INTO logiciel VALUES (sequenceLog.NEXTVAL,'log3','SQL Server', '12-04-1998','7','PCNT',3000);
+INSERT INTO logiciel VALUES (sequenceLog.NEXTVAL,'log4','Front Page', '03-06-1997','5','PCWS',500);
+INSERT INTO logiciel VALUES (sequenceLog.NEXTVAL,'log5','WinDev',     '12-05-1997','5','PCWS',750);
+INSERT INTO logiciel VALUES (sequenceLog.NEXTVAL,'log6','SQL*Net', NULL, '2.0','UNIX',500);
+INSERT INTO logiciel VALUES (sequenceLog.NEXTVAL,'log7','I. I. S.',   '12-04-2002','2','PCNT',900);
+INSERT INTO logiciel VALUES (sequenceLog.NEXTVAL,'log8','DreamWeaver','21-09-2003','2.0','BeOS',1400);
+
+
+INSERT INTO Types VALUES (sequenceTyp.NEXTVAL,'TX',  'Terminal X-Window');
+INSERT INTO Types VALUES (sequenceTyp.NEXTVAL,'UNIX','Système Unix');
+INSERT INTO Types VALUES (sequenceTyp.NEXTVAL,'PCNT','PC Windows  NT');
+INSERT INTO Types VALUES (sequenceTyp.NEXTVAL,'PCWS','PC Windows');
+INSERT INTO Types VALUES (sequenceTyp.NEXTVAL,'NC',  'Network Computer');
+
+INSERT INTO installer VALUES ('p2', 'log1', sequenceIns.NEXTVAL,'15-05-2003',NULL);
+INSERT INTO installer VALUES ('p2', 'log2', sequenceIns.NEXTVAL,'17-09-2003',NULL);
+INSERT INTO installer VALUES ('p4', 'log5', sequenceIns.NEXTVAL, NULL,NULL);
+INSERT INTO installer VALUES ('p6', 'log6', sequenceIns.NEXTVAL,'20-05-2003',NULL);
+INSERT INTO installer VALUES ('p6', 'log1', sequenceIns.NEXTVAL,'20-05-2003',NULL);
+INSERT INTO installer VALUES ('p8', 'log2', sequenceIns.NEXTVAL,'19-05-2003',NULL);
+INSERT INTO installer VALUES ('p8', 'log6', sequenceIns.NEXTVAL,'20-05-2003',NULL);
+INSERT INTO installer VALUES ('p11','log3', sequenceIns.NEXTVAL,'20-04-2003',NULL);
+INSERT INTO installer VALUES ('p12','log4', sequenceIns.NEXTVAL,'20-04-2003',NULL);
+INSERT INTO installer VALUES ('p11','log7', sequenceIns.NEXTVAL,'20-04-2003',NULL);
+INSERT INTO installer VALUES ('p7', 'log7', sequenceIns.NEXTVAL,'01-04-2002',NULL);
+
+COMMIT;
+
+SELECT * FROM Segment;
+
+SELECT * FROM Salle;
+
+SELECT * FROM Poste;
+
+SELECT * FROM Logiciel;
+
+SELECT * FROM Installer;
+
+SELECT * FROM Types;
+
+
