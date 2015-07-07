d7ad75fea794b0b8ca231ffcb2838cf4079e9085
NadiaUQAM <nadia_tahiri@yahoo.fr>
2015-07-07 18:18:21 -0400

    Lab9

--- Lab/Lab9_sol/question3_lab9.sql
+++ Lab/Lab9_sol/question3_lab9.sql
@@ -5,25 +5,32 @@ CREATE OR REPLACE PROCEDURE tourMonde
 	CURSOR curseur3 IS 
 		SELECT ville_depart, ville_arrivee
 		FROM vol;
-	volD vol.ville_depart%TYPE;
-	volA vol.ville_arrivee%TYPE;
 	volCourant vol.ville_depart%TYPE:=volDepart;
+	iter integer :=1;
 BEGIN
-     OPEN curseur3; 
-		DBMS_OUTPUT.PUT_LINE('VOTRE ITENERAIRE:');
-		FOR ligne IN curseur3 LOOP
-			DBMS_OUTPUT.PUT_LINE('A partir de ' ||  ligne.volD ||
-							  ' .');
-			IF (ligne.ville_depart=volCourant) THEN
-			DBMS_OUTPUT.PUT_LINE('A partir de ' || volD ||
-							  ' vers' || volA ||
-							  ' .');
-			volCourant:=ligne.ville_arrivee;			
-			END IF;
-		END LOOP;
+	DBMS_OUTPUT.PUT_LINE('VOTRE ITENERAIRE:');
+	FOR ligne IN curseur3 LOOP
+		IF (ligne.ville_depart=volCourant) THEN
+			DBMS_OUTPUT.PUT_LINE('Vol '|| iter ||': de ' ||ligne.ville_depart ||' vers ' || ligne.ville_arrivee ||'.');
+			volCourant:=ligne.ville_arrivee;	
+			iter := iter + 1;
+		END IF;
+	END LOOP;
 
-	 CLOSE curseur3; 
-		DBMS_OUTPUT.PUT_LINE('FIN ITENERAIRE DU TOUR DU MONDE. BON VOYAGE!');
+	DBMS_OUTPUT.PUT_LINE('FIN ITENERAIRE DU TOUR DU MONDE. BON VOYAGE!');
+END;
+/
+
+CREATE OR REPLACE FUNCTION obtenirListeVilleD 
+(volDepart vol.ville_depart%TYPE) RETURN SYS_REFCURSOR IS
+cur SYS_REFCURSOR;
+
+BEGIN
+	OPEN cur FOR 
+				SELECT ville_depart, ville_arrivee
+				FROM vol
+				WHERE ville_depart=volDepart;
+	RETURN cur;
 END;
 /
 execute tourMonde('Montreal');
